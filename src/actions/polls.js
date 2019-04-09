04827cd19835c91df202bcda28a1d082ed8924b2
virmon <virmontumulak@gmail.com>
2019-04-09 15:41:03 +0800

    feat: update actions

--- src/actions/polls.js
+++ src/actions/polls.js
@@ -1,8 +1,40 @@
-export const RECEIVE_POLL = 'RECEIVE_POLLS'
+import { savePoll } from '../utils/api'
 
-export function receivePolls (poll) {
+export const RECEIVE_POLL = 'RECEIVE_POLLS'
+export const ADD_POLL = 'ADD_POLL'
+export const ANSWER_POLL = 'ANSWER_POLL'
+
+export function receivePolls (polls) {
     return {
         type: RECEIVE_POLLS,
+        polls
+    }
+}
+
+export function addPoll (poll) {
+    return {
+        type: ADD_POLL,
         poll
+    }
+}
+
+export function handleAddPoll (poll) {
+    return (dispatch, getState) => {
+        const { authedUser } = getState()
+
+        savePoll({
+            poll,
+            author: authedUser
+        })
+            .then((poll) => dispatch(addPoll(poll)))
+    }
+}
+
+export function answerPoll ({ id, authedUser, answer }) {
+    return {
+        type: ANSWER_POLL,
+        id,
+        authedUser,
+        answer
     }
 }
--- src/actions/shared.js
+++ src/actions/shared.js
@@ -3,7 +3,7 @@ import { receivePolls } from './polls'
 import { receiveUsers } from './users'
 import { setAuthedUser } from './authedUser';
 
-const AUTHED_USER = 'user'
+const AUTHED_ID = 'sarahedo'
 
 export function handleInitialData () {
     return (dispatch) => {
@@ -11,7 +11,7 @@ export function handleInitialData () {
             .then(({polls, users}) => {
                 dispatch(receivePolls(polls))
                 dispatch(receiveUsers(users))
-                dispatch(setAuthedUser(AUTHED_USER))
+                dispatch(setAuthedUser(AUTHED_ID))
             })
     }
 }
