ad950cc53c116bae7297b83554ff0ef3331c3f8d
virmon <virmontumulak@gmail.com>
2019-04-12 21:39:08 +0800

    fix: sort score to decending order

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -30,6 +30,9 @@ class Leaderboard extends Component {
 
 function mapStateToProps ({ authedUser, users }) {
     const userIds = Object.keys(users)
+                        .sort((a, b) => 
+                            (Object.keys(users[b].answers).length+users[b].questions.length) - (Object.keys(users[a].answers).length+users[a].questions.length))
+
     return {
         authedUser,
         users,
