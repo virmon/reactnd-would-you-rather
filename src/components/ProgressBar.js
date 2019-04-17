461dcc1c49b6e0da81083ad1933a3f0d08d74756
virmon <virmontumulak@gmail.com>
2019-04-17 12:46:35 +0800

    refactor: change into stateless component

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,20 +1,18 @@
 import React, { Component } from 'react'
 
-class ProgressBar extends Component {
-    render () {
-        const { score }  = this.props
-        const percent = score
-        return (
-            <div className='progress-bar'>
-                <div 
-                    className='fill'
-                    style={{width:`${percent}%`}}
-                >
-                    {percent}%
-                </div>
+const ProgressBar = () => {
+    const { score }  = this.props
+    const percent = score
+    return (
+        <div className='progress-bar'>
+            <div 
+                className='fill'
+                style={{width:`${percent}%`}}
+            >
+                {percent}%
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default ProgressBar
