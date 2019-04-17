689262ccfc14fab8ff579022b174e39a819e371f
virmon <virmontumulak@gmail.com>
2019-04-17 12:41:58 +0800

    fix: add answered poll in users state

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,7 +1,16 @@
 import { RECEIVE_USERS, SAVE_ANSWERED_POLL } from '../actions/users'
+import { ADD_POLL } from '../actions/polls'
 
 export default function users (state = {}, action) {
     switch(action.type) {
+        case ADD_POLL :
+            return {
+                ...state,
+                [action.poll.author]: {
+                    ...state[action.poll.author],
+                    questions: state[action.poll.author].questions.concat(action.poll.id)
+                }
+            }
         case RECEIVE_USERS :
             return {
                 ...state,
