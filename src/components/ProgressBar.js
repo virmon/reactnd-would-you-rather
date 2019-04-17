57733c7cf219688eb699ffde7284438c3873c443
virmon <virmontumulak@gmail.com>
2019-04-17 20:08:19 +0800

    refactor: unpack field in function parameter

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,7 +1,6 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-const ProgressBar = () => {
-    const { score }  = this.props
+const ProgressBar = ({score}) => {
     const percent = score
     return (
         <div className='progress-bar'>
